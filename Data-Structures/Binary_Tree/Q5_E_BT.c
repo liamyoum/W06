8c0ff64153eb9a01a0d347aab8aa5397c0c70195
liamyoum <waiting4corona@gmail.com>
2026-04-07 15:40:59 +0900

    Implemented mirrorTree()

--- Data-Structures/Binary_Tree/Q5_E_BT.c
+++ Data-Structures/Binary_Tree/Q5_E_BT.c
@@ -106,6 +106,16 @@ int main()
 void mirrorTree(BTNode *node)
 {
 	/* add your code here */
+    if (node == NULL) {
+        return;
+    }
+
+    BTNode *temp = node->left;
+    node->left = node->right;
+    node->right = temp;
+
+    mirrorTree(node->right);
+    mirrorTree(node->left);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
